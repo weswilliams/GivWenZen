35d772cf1e68b3288bfea6d2a5891b88b8e2818c
weswilliams <wes.williamz@gmail.com>
2009-10-07 02:29:12 +0000

    - fixed tests that had duplicate steps -- this does not fix the issues that allowed the duplicate steps (Kelly has a fix for this)

--- src/test/java/bdd/steps/SimpleTestSteps.java
+++ src/test/java/bdd/steps/SimpleTestSteps.java
@@ -13,7 +13,7 @@ public class SimpleTestSteps {
   private GivWenZen gwz;
   private Map<String, Boolean> stepCalls = new HashMap<String, Boolean>();
   private Object paramValue;
-  
+
   public SimpleTestSteps(GivWenZen gwz) {
     this.gwz = gwz;
   }
@@ -22,44 +22,44 @@ public class SimpleTestSteps {
   public void verifyStepExists(String step) throws Exception {
     gwz.given(step);
   }
- 
+
   @DomainStep("the step '(.*)' is called")
   public void callStep(String stepToCall) throws Exception {
     gwz.when(stepToCall);
   }
-  
+
   @DomainStep("the step '(.*)' executes successfully")
   public boolean stepExecutedSuccessfully(String executedStep) {
     return stepCalls.get(executedStep).equals(true);
   }
-  
+
   @DomainStep("simple no parameter step")
   public void simpleNoParamStep() {
     stepCalls.put("simple no parameter step", true);
   }
-  
+
   @DomainStep("simple step with int parameter (\\d+)")
   public void simpleIntParamTest(int intParam) {
     stepCalls.put("simple step with int parameter " + intParam, true);
     paramValue = intParam;
   }
-  
+
   @DomainStep("simple step with (.*) parameter")
   public void simpleCustomTypeParamTest(CustomType customType) {
     stepCalls.put("simple step with CustomType parameter", true);
     paramValue = customType;
   }
-  
+
   @DomainStep("the value (\\d+) is passed as a parameter")
   public boolean verifyParamValue(int paramValue) {
     return this.paramValue.equals(paramValue);
   }
-  
-  @DomainStep("the value (.*) is passed as a parameter")
-  public void domainStep(CustomType customType) {
-    customType.equals(paramValue);
+
+  @DomainStep("the custom value (.*) is passed as a parameter")
+  public boolean veryifyCustomTypeParam(CustomType customType) {
+    return customType.equals(paramValue);
   }
-  
+
   @DomainStep("the CustomType has a CustomTypeEditor in the same package as the CustomType")
   public void checkTheCustomTypeHasAPropertyEditor() {
     assert(CustomType.class.getPackage().equals(CustomTypeEditor.class.getPackage()));
