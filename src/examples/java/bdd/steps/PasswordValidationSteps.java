61624e2451f7da1a5c49e59863af5d81d0805356
weswilliams <wes.williamz@gmail.com>
2010-01-18 07:23:01 +0000

    - finished pw examples - changed intellij project to use multiple modules and build in the same format as ant build

--- src/examples/java/bdd/steps/PasswordValidationSteps.java
+++ src/examples/java/bdd/steps/PasswordValidationSteps.java
@@ -6,53 +6,50 @@ import org.givwenzen.annotations.DomainSteps;
 @DomainSteps
 public class PasswordValidationSteps {
 
-    private static final String START_OF_LINE = "^";
-    private static final String ANY_CHARS_AND_END_OF_LINE = ".*$";
-    private static final String AT_LEAST_ONE_ALPHA_CHAR = "(?=.*[a-zA-Z])";
-    private static final String AT_LEAST_ONE_SPECIAL_CHAR = "(?=.*\\W)";
-    private static final String AT_LEAST_ONE_DIGIT = "(?=.*\\d)";
-    private static final String NO_MORE_THAN_16_CHARS = "(?!.{17,}$)";
-    private static final String AT_LEAST_6_CHARS = "(?=.{6,}$)";
-    private static final String DOES_NOT_CONTAIN_SPACES = "(?!.* )";
-    private String passwordToVerify = "";
+   private static final String START_OF_LINE = "^";
+   private static final String ANY_CHARS_AND_END_OF_LINE = ".*$";
+   private static final String AT_LEAST_ONE_ALPHA_CHAR = "(?=.*[a-zA-Z])";
+   private static final String AT_LEAST_ONE_SPECIAL_CHAR = "(?=.*\\W)";
+   private static final String AT_LEAST_ONE_DIGIT = "(?=.*\\d)";
+   private static final String NO_MORE_THAN_16_CHARS = "(?!.{17,}$)";
+   private static final String AT_LEAST_6_CHARS = "(?=.{6,}$)";
+   private static final String DOES_NOT_CONTAIN_SPACES = "(?!.* )";
+   private String passwordToVerify = "";
 
-    @DomainStep("a new account is created with a password of (.*)")
-    public String createAccountWithPassword(String password) {
-        changePasswordForExistingAccount(password);
-        return password;
-    }
-    
-    @DomainStep("the password is changed to (.*)")
-    public void changePasswordForExistingAccount(String newPassword) {
-        this.passwordToVerify = newPassword == null ? "" : newPassword;
-    }
-    
-    @DomainStep("the password is accepted because it meets the security requirements")
-    public boolean passwordMeetsSecurityRequirements() {
-      return isValidPassword();
-    }
-    
-    @DomainStep("the password is rejected (?:.*)")
-    public boolean isInvalidPassword() {
-        return !isValidPassword();
-    }
-    
-    @DomainStep("the password is valid (?:.*)")
-    public boolean isValidPassword() {
-        System.out.println(passwordToVerify);
-        return passwordToVerify.matches( 
-                        START_OF_LINE +
-                        AT_LEAST_6_CHARS + 
-                        NO_MORE_THAN_16_CHARS + 
-                        AT_LEAST_ONE_DIGIT + 
-                        AT_LEAST_ONE_SPECIAL_CHAR + 
-                        AT_LEAST_ONE_ALPHA_CHAR + 
-                        DOES_NOT_CONTAIN_SPACES +
-                        ANY_CHARS_AND_END_OF_LINE);
-    }
+   @DomainStep("a new account is created with a password of (.*)")
+   public String createAccountWithPassword(String password) {
+      this.passwordToVerify = password == null ? "" : password;
+      return password;
+   }
 
-    @DomainStep("the new account is not created")
-    public boolean newAccountIsNotCreated() {
-        return true;
-    }
+   @DomainStep("the password is accepted (?:.*)")
+   public boolean passwordMeetsSecurityRequirements() {
+      return isAcceptedPassword();
+   }
+
+   @DomainStep("the password is rejected (?:.*)")
+   public boolean isRejectedPassword() {
+      return !isAcceptedPassword();
+   }
+
+   @DomainStep("a new account is created with password (.*) then it is rejected because (?:.*)")
+   public boolean createAccountWithInvalidPasswordAndVerifyItIsRejected(String password) {
+      createAccountWithPassword(password);
+      return isRejectedPassword();
+   }
+
+   @DomainStep("the password is valid (?:.*)")
+   public boolean isAcceptedPassword() {
+      System.out.println(passwordToVerify);
+      return passwordToVerify.matches(
+         START_OF_LINE +
+            AT_LEAST_6_CHARS +
+            NO_MORE_THAN_16_CHARS +
+            AT_LEAST_ONE_DIGIT +
+            AT_LEAST_ONE_SPECIAL_CHAR +
+            AT_LEAST_ONE_ALPHA_CHAR +
+            DOES_NOT_CONTAIN_SPACES +
+            ANY_CHARS_AND_END_OF_LINE);
+   }
+
 }
