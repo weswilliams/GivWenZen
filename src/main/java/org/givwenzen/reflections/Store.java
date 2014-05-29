bce4cc2714a00a2f9f433328d87d1607ecd14e61
Andrew Townsend <andrew.townsend@resonateinsights.com>
2014-05-29 14:50:37 -0400

    Fixed the deprecated call to MapMaker().makeComputingMap - that method is now private and inaccessible.  Updated guava while I was at it.

--- src/main/java/org/givwenzen/reflections/Store.java
+++ src/main/java/org/givwenzen/reflections/Store.java
@@ -2,10 +2,10 @@ package org.givwenzen.reflections;
 
 import com.google.common.base.Function;
 import com.google.common.base.Supplier;
-import com.google.common.collect.MapMaker;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.CacheBuilder;
 
 import java.util.Collection;
 import java.util.Map;
@@ -15,22 +15,26 @@ import java.util.Set;
  *
  */
 public class Store {
-
-	final Map<String/*indexName*/, Multimap<String, String>> store = new MapMaker().makeComputingMap(new Function<String, Multimap<String, String>>() {
-		public Multimap<String, String> apply(String indexName) {
-			return Multimaps.newSetMultimap(new MapMaker().<String, Collection<String>>makeMap(), new Supplier<Set<String>>() {
-				public Set<String> get() {
-					return Sets.newHashSet();
-				}
-			});
-		}
-	});
+    final Cache<String, Multimap<String, String>> store = CacheBuilder.newBuilder().build(
+            new CacheLoader<String, Multimap<String, String>>() {
+                    public Multimap<String, String> load(String key) {
+                        return Multimaps.newSetMultimap(new MapMaker().<String, Collection<String>>makeMap(), new Supplier<Set<String>>() {
+                            public Set<String> get() {
+                                return Sets.newHashSet();
+                            }
+                        });
+                    }
+            });
 
     /** get all values of given keys from given indexName */
 	public Set<String> get(String indexName, String... keys) {
         Set<String> result = Sets.newHashSet();
 
-        Multimap<String, String> map = store.get(indexName);
+        Multimap<String, String> map = store.getIfPresent(indexName);
+        if(map == null) {
+            map = ArrayListMultimap.create();
+            store.put(indexName, map);
+        }
         for (String key : keys) {
             result.addAll(map.get(key));
         }
@@ -39,13 +43,13 @@ public class Store {
 	}
 
     Multimap<String, String> getStore(String indexName) {
-        return store.get(indexName);
+        return store.getIfPresent(indexName);
     }
 
     /** merges given store into this */    
 	public void merge(final Store outer) {
-		for (String indexName : outer.store.keySet()) {
-			this.store.get(indexName).putAll(outer.store.get(indexName));
+		for (String indexName : outer.store.asMap().keySet()) {
+			this.store.getIfPresent(indexName).putAll(outer.store.getIfPresent(indexName));
 		}
 	}
 }
